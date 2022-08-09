9832e23cf1da7d40975588e2ffa8f1eed37a904a
carlo <carlogbossou93@gmail.com>
2022-08-09 18:44:25 +0000

    09/08/2022

--- src/com/bookstore/entities/Category.java
+++ src/com/bookstore/entities/Category.java
@@ -15,7 +15,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "category")
+@Table(name = "category", catalog="heroku_03ee648e7239c63")
 @NamedQueries({
 	@NamedQuery(name = "Category.findAll", query= "SELECT c FROM Category c ORDER BY c.categoryId"),
 	@NamedQuery(name = "Category.countAll", query = "SELECT COUNT(*) FROM Category"),
